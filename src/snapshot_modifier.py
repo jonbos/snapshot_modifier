09cb5ba8b98d4a4dd400f04537e771b151e0a142
Jon Bos <jbos@mumosystems.com>
2021-07-29 21:47:02 -0500

    updated mapping classes to use bs4 for order-agnostic attribute mapping

--- src/snapshot_modifier.py
+++ src/snapshot_modifier.py
@@ -1,38 +1,45 @@
 import argparse
 import csv
 import os
-import pathlib
 import shutil
 import zipfile
 from pathlib import Path
 
-from src.mapping_functions import GroupMappingFunction, UserMappingFunction, ResolutionMappingFunction, MappingFunction
+from bs4 import BeautifulSoup as bs
 
-OUTPUT_DIR = "./snapshots/modified_snapshots"
+from src.mapping import GroupMapping, UserMapping, ResolutionMapping, Mapping
+
+OUTPUT_DIR = "../snapshots/modified_snapshots"
 
 
-def get_snapshot_file_dict(snapshot_directory):
-    zips_in_dir = filter(lambda file: file.split('.')[-1].lower() == "zip", os.listdir(snapshot_directory))
-    snapshot_dict = {}
-    for zip_file in zips_in_dir:
-        base_name = os.path.splitext(zip_file)[0]
-        zip_dict = {
-            "zip_location": os.path.join(os.path.abspath(snapshot_directory), zip_file),
-            "unzip_location": os.path.join(os.path.abspath(snapshot_directory), base_name)
-        }
-        snapshot_dict[base_name] = zip_dict
-    return snapshot_dict
+def _get_file_basename(zip_filepath):
+    return os.path.splitext(os.path.basename(zip_filepath))[0]
+
+
+def _get_zip_location(zip_filepath):
+    return os.path.abspath(zip_filepath)
+
+
+def _get_unzip_location(zip_filepath):
+    return os.path.join(os.path.abspath(os.path.dirname(zip_filepath)), _get_file_basename(zip_filepath))
+
+
+def get_snapshot_file_dict(snapshot_zips):
+    return {
+        _get_file_basename(zip_file): {"zip_location": _get_zip_location(zip_file),
+                                       "unzip_location": _get_unzip_location(zip_file)}
+        for zip_file in snapshot_zips}
 
 
 def unzip_snapshots(snapshot_dict):
-    for zip_name, zip_dict in snapshot_dict.items():
+    for _, zip_dict in snapshot_dict.items():
         with zipfile.ZipFile(zip_dict["zip_location"], "r") as zip_ref:
             zip_ref.extractall(zip_dict["unzip_location"])
 
 
 def get_xml_files_from_snapshot_directories(snapshot_dict):
     xmls = []
-    for snap_name, snap_info in snapshot_dict.items():
+    for _, snap_info in snapshot_dict.items():
         xmls += [os.path.join(snap_info["unzip_location"], file) for file in os.listdir(snap_info["unzip_location"]) if
                  file.endswith(".xml")]
     return xmls
@@ -47,11 +54,12 @@ def create_output_dir():
 
 def update_xml_file(xml_file, mapping_list):
     path = Path(xml_file)
-    text = path.read_text()
+    soup = bs(path.read_text(), 'xml')
 
     for mapping_function in mapping_list:
-        text = mapping_function.apply(text)
-    path.write_text(text)
+        mapping_function.apply(soup)
+
+    path.write_text(soup.prettify())
 
 
 def update_xmls(xml_files, mapping_list):
@@ -71,9 +79,45 @@ def rezip_directories(snapshot_dict):
                 zip_out.write(file, os.path.basename(file))
 
 
+def remove_unzipped_directories(snapshot_dict):
+    for snapshot_name, snapshot_info in snapshot_dict.items():
+        shutil.rmtree(snapshot_info["unzip_location"])
+
+
+def create_group_mappings(group_mapping_file) -> list[Mapping]:
+    group_reader = csv.DictReader(group_mapping_file) if group_mapping_file is not None else []
+    return [GroupMapping(row) for row in group_reader]
+
+
+def create_user_mappings(user_mapping_file) -> list[Mapping]:
+    user_reader = csv.DictReader(user_mapping_file) if user_mapping_file is not None else []
+    return [UserMapping(row) for row in user_reader]
+
+
+def create_resolution_mappings(resolution_mapping_file) -> list[Mapping]:
+    resolution_reader = csv.DictReader(resolution_mapping_file) if resolution_mapping_file is not None else []
+    return [ResolutionMapping(row) for row in resolution_reader]
+
+
+def create_mapping_list(parsed_args) -> list[Mapping]:
+    return create_group_mappings(parsed_args.group_mapping_file) \
+           + create_user_mappings(parsed_args.user_mapping_file) \
+           + create_resolution_mappings(parsed_args.resolution_mapping_file)
+
+
+def modify_snapshots(parsed_args):
+    snapshot_dict = get_snapshot_file_dict(parsed_args.snapshot_zips)
+    mapping_list = create_mapping_list(parsed_args)
+    unzip_snapshots(snapshot_dict)
+    xml_files = get_xml_files_from_snapshot_directories(snapshot_dict)
+    update_xmls(xml_files, mapping_list)
+    rezip_directories(snapshot_dict)
+    remove_unzipped_directories(snapshot_dict)
+
+
 def initialize_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("snapshot_dir", type=pathlib.Path,
+    parser.add_argument("snapshot_zips", nargs="+",
                         help="Path to directory containing the snapshots to be modified")
     parser.add_argument("-g", "--group_mapping_file", type=argparse.FileType('r'), default=None,
                         help="CSV file containing mapping instructions for group names")
@@ -81,45 +125,8 @@ def initialize_args():
                         help="CSV file containing mapping instructions for user names")
     parser.add_argument("-r", "--resolution_mapping_file", type=argparse.FileType('r'), default=None,
                         help="CSV file containing mapping instructions for resolutions")
-    return parser.parse_args("-g ./group_mapping.csv -u ./user_mapping.csv ./snapshots/test_snapshots".split())
-
-
-def remove_unzipped_directories(snapshot_dict):
-    for snapshot_name, snapshot_info in snapshot_dict.items():
-        shutil.rmtree(snapshot_info["unzip_location"])
-
-
-def create_group_mappings(group_mapping_file):
-    group_reader = csv.DictReader(group_mapping_file) if group_mapping_file is not None else []
-    return [GroupMappingFunction(row) for row in group_reader]
-
-
-def create_user_mappings(user_mapping_file):
-    user_reader = csv.DictReader(user_mapping_file) if user_mapping_file is not None else []
-    return [UserMappingFunction(row) for row in user_reader]
-
-
-def create_resolution_mappings(resolution_mapping_file):
-    resolution_reader = csv.DictReader(resolution_mapping_file) if resolution_mapping_file is not None else []
-    return [ResolutionMappingFunction(row) for row in resolution_reader]
-
-
-def create_mapping_list(group_mapping_file, user_mapping_file, resolution_mapping_file) -> list[MappingFunction]:
-    return create_group_mappings(group_mapping_file) \
-           + create_user_mappings(user_mapping_file) \
-           + create_resolution_mappings(resolution_mapping_file)
-
-
-def modify_snapshots(parsed_args):
-    snapshot_dict = get_snapshot_file_dict(parsed_args.snapshot_dir)
-    mapping_list = create_mapping_list(group_mapping_file=parsed_args.group_mapping_file,
-                                       user_mapping_file=parsed_args.user_mapping_file,
-                                       resolution_mapping_file=parsed_args.resolution_mapping_file)
-    unzip_snapshots(snapshot_dict)
-    xml_files = get_xml_files_from_snapshot_directories(snapshot_dict)
-    update_xmls(xml_files, mapping_list)
-    rezip_directories(snapshot_dict)
-    remove_unzipped_directories(snapshot_dict)
+    return parser.parse_args(
+        "-g ../mapping_files/group_mapping.csv -u ../mapping_files/user_mapping.csv ../snapshots/test_snapshots/kmj.zip".split())
 
 
 if __name__ == '__main__':
